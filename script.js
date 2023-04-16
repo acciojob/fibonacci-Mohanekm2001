da43ef767f18cc54431bbbfbb1482997a9e77a6e
Mohanekm2001 <mohan2001ekm@gmail.com>
2023-04-16 09:55:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,14 +4,6 @@ function fibonacci(num) {
   } else if (num === 1) {
     return 1;
   } else {
-    let prev1 = 0;
-    let prev2 = 1;
-    let fibNum = 0;
-    for (let i = 2; i <= num; i++) {
-      fibNum = prev1 + prev2;
-      prev1 = prev2;
-      prev2 = fibNum;
-    }
-    return fibNum;
+    return fibonacci(num - 1) + fibonacci(num - 2);
   }
 }
